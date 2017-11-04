8ad4597ebd07321dd9243b1a7f714e61a5a4b8ee
Timur @shako Kashapov <timshako@gmail.com>
2017-11-05 02:37:34 +0300

    upd

--- geography/src/main/java/Station.java
+++ geography/src/main/java/Station.java
@@ -1,2 +1,16 @@
+/**
+ * Станция.
+ *
+ * @author Timur Kashapov
+ * @version 0.0.1
+ * @since 2017
+ */
 public class Station {
+
+    /**
+     *
+     */
+    public Station() {
+        // TODO:
+    }
 }
