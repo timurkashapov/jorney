2549af0957307634735cf421f3bcd33af7ab0a95
Timur @shako Kashapov <timshako@gmail.com>
2017-11-05 23:51:31 +0300

    Переопределяю методы equals() и hashcode() для классы Station

--- geography/src/main/java/Station.java
+++ geography/src/main/java/Station.java
@@ -55,4 +55,26 @@ public class Station {
             return new Station();
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Station station = (Station) o;
+
+        if (address != null ? !address.equals(station.address) : station.address != null) return false;
+        if (coordinates != null ? !coordinates.equals(station.coordinates) : station.coordinates != null) return false;
+        if (location != null ? !location.equals(station.location) : station.location != null) return false;
+        return phone != null ? phone.equals(station.phone) : station.phone == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = address != null ? address.hashCode() : 0;
+        result = 31 * result + (coordinates != null ? coordinates.hashCode() : 0);
+        result = 31 * result + (location != null ? location.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        return result;
+    }
 }
